3567d5c2e0d0a1ac419da39d0d2b98de23bbde4c
Natalya Karpova <knataly0513@gmail.com>
2022-12-03 17:11:55 +0200

    Update modal.js

--- src/js/dom/modal.js
+++ src/js/dom/modal.js
@@ -1,2 +1,24 @@
 // функції відкриття та закриття з modal.js
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
 // + рендер карток фільмів
+// function addModalFilmcardMarkup() {
+//     const modalWrapper = document.querySelector('.modal-wrapper');
+//     const markup = ''
+//     modalWrapper.insertAdjacentHTML(afterbegin, markup);
+// }
