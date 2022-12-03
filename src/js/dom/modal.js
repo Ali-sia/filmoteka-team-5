dd3a838385385dce913678006cbf7dd8fd3e0957
Natalya Karpova <knataly0513@gmail.com>
2022-12-03 17:12:41 +0200

    Update modal.js

--- src/js/dom/modal.js
+++ src/js/dom/modal.js
@@ -1,21 +1,21 @@
 // функції відкриття та закриття з modal.js
 
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    // document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleModal() {
+//     // document.body.classList.toggle("modal-open");
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
 // + рендер карток фільмів
 // function addModalFilmcardMarkup() {
 //     const modalWrapper = document.querySelector('.modal-wrapper');
