3bc85616686d4505f2f119f8c0cccbd1b6213f96
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-12-04 18:19:56 +0200

    Added overflow hidden.

--- src/js/dom/modal.js
+++ src/js/dom/modal.js
@@ -88,7 +88,8 @@ function makeFilmcardMarkup(filmData) {
 }
 
 function openModal () {
-    backdrop.classList.remove("is-hidden")
+    backdrop.classList.remove("is-hidden");
+    document.body.style.overflow = 'hidden';
     modalCloseBtn.addEventListener("click", closeModal);
     document.addEventListener("click", closeModalByOutBackdropClick);
     document.addEventListener("keydown", closeModalByEsc);
@@ -96,6 +97,7 @@ function openModal () {
 
 function closeModal() {
     backdrop.classList.add("is-hidden");
+    document.body.style.overflow = 'overlay';
     modalCloseBtn.removeEventListener("click", closeModal);
     document.removeEventListener("click", closeModalByOutBackdropClick);
     document.removeEventListener("keydown", closeModalByEsc);
