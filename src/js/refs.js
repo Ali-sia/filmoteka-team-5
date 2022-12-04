d30aae1fd58a2a1bdfbd1eb8b245fafeba8b800f
Svitlana Tymoshevska <amelia.tsa@gmail.com>
2022-12-04 09:22:11 +0200

    Changed modalEl in function makeFilmcardMarkup.

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,12 +3,14 @@ const refs = {
     exampleEl: document.querySelector('.example'),
     list: document.querySelector('.films__list'),
     backdrop: document.querySelector('.backdrop'),
+    modalCloseBtn: document.querySelector(".modal-close-btn")
 };
 
 export const {
     exampleEl,
     list,
     backdrop,
+    modalCloseBtn,
 } = refs;
 
 // тоді при import пишемо просто
