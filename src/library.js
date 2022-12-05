463a1ec0df827ebadb68fc00d7147c4e9ad52586
KychataOlena <106249466+KychataOlena@users.noreply.github.com>
2022-12-06 00:06:40 +0200

    add queue library

---
+++ src/library.js
@@ -0,0 +1,8 @@
+import './js/dom/show-queue-films';
+import './js/dom/loader';
+import { activePage } from './js/dom/activePage';
+activePage();
+import { activeLibraryBtn } from './js/dom/activeLibraryBtn';
+activeLibraryBtn();
+
+import './js/dom/modal';
