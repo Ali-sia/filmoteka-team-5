0e304e9763d979a6547eac3b0be75d9e2f92222c
Volodymyr-Zastavniuk <106408963+Volodymyr-Zastavniuk@users.noreply.github.com>
2022-12-06 22:45:38 +0200

    update scroll behavior

--- src/js/dom/scroll-btn.js
+++ src/js/dom/scroll-btn.js
@@ -1,22 +1,11 @@
 import { btnScroll } from '../refs';
 
-// const btnScroll = document.querySelector('.scroll-btn');
-btnScroll.addEventListener('click', topFunction);
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+window.addEventListener('scroll', onWindowScroll)
 
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+function onWindowScroll() {
+  if (scrollY > 800) {
     btnScroll.style.display = 'flex';
   } else {
     btnScroll.style.display = 'none';
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+};
