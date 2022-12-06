0069139f980cf55a20a6e247ad71bbe58a37f808
aliesia-zahorui <aliesia@ukr.net>
2022-12-06 02:00:48 +0200

    1

---
+++ src/js/dom/modal-team.js
@@ -0,0 +1,4 @@
+import { modalTeamBtn } from '../refs';
+import { openModal } from './modal';
+
+modalTeamBtn.addEventListener('click', openModal);
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,18 +1,20 @@
 //сюди всі змінні
 const refs = {
-    exampleEl: document.querySelector('.example'),
-    list: document.querySelector('.films__list'),
-    backdrop: document.querySelector('.backdrop'),
-    filmcard: document.querySelector('.filmcard'),
-    modalCloseBtn: document.querySelector(".modal-close-btn")
+  exampleEl: document.querySelector('.example'),
+  list: document.querySelector('.films__list'),
+  backdrop: document.querySelector('.backdrop'),
+  filmcard: document.querySelector('.filmcard'),
+  modalCloseBtn: document.querySelector('.modal-close-btn'),
+  modalTeamBtn: document.querySelector('.footer__button'),
 };
 
 export const {
-    exampleEl,
-    list,
-    backdrop,
-    filmcard,
-    modalCloseBtn,
+  exampleEl,
+  list,
+  backdrop,
+  filmcard,
+  modalCloseBtn,
+  modalTeamBtn,
 } = refs;
 
 // тоді при import пишемо просто
