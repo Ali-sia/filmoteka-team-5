215b4ddc93ba2d62405997f0b89a071352caab03
Volodymyr-Zastavniuk <106408963+Volodymyr-Zastavniuk@users.noreply.github.com>
2022-12-07 10:59:30 +0200

    update-library-page-2

--- src/js/dom/show-watch-films.js
+++ src/js/dom/show-watch-films.js
@@ -3,27 +3,9 @@ import { resetMarkup } from './show-popular-films';
 import { appendPopularMarkup } from './show-popular-films';
 import WatchedFilmsStorage from '../storage/add-to-watced';
 
-import { getElement } from './getElement';
-
-if (getElement('.header__libraryWatchBtn')) {
-  getElement('.header__libraryWatchBtn').addEventListener(
-    'click',
-    onWatchedLibClick
-  );
+export const watchedFilmsStorage = new WatchedFilmsStorage;
+export function onWatchedLibClick() {
+    const watchedList = watchedFilmsStorage.getWathedFilmsList();
+    resetMarkup();
+    appendPopularMarkup(watchedList);
 }
-
-const watchedFilmsStorage = new WatchedFilmsStorage();
-const watchedList = watchedFilmsStorage.getWathedFilmsList();
-
-// const watchedBtnLib = document.querySelector('.header__libraryWatchBtn');
-// console.log(watchedBtnLib);
-
-// watchedBtnLib.addEventListener('click', onWatchedLibClick);
-
-function onWatchedLibClick() {
-  console.log(watchedList);
-  resetMarkup();
-  createPopularMarkup(watchedList);
-  appendPopularMarkup(watchedList);
-}
-
