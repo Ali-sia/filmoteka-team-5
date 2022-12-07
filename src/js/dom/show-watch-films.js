0a4e21369ec77400b5c0f4e2df64e41b9b8db832
Volodymyr-Zastavniuk <106408963+Volodymyr-Zastavniuk@users.noreply.github.com>
2022-12-07 04:32:46 +0200

    fix library page and modal

--- src/js/dom/show-watch-films.js
+++ src/js/dom/show-watch-films.js
@@ -1,26 +1,11 @@
 // відмалювати фільми з списку "watched"
 import { resetMarkup } from './show-popular-films';
 import { appendPopularMarkup } from './show-popular-films';
-import { createPopularMarkup } from './show-popular-films';
 import WatchedFilmsStorage from '../storage/add-to-watced';
-import { getElement } from "./getElement";
 
-    if (getElement(".header__libraryWatchBtn")) {
-        getElement(".header__libraryWatchBtn").addEventListener('click', onWatchedLibClick);
-    }
-
-
-const watchedFilmsStorage = new WatchedFilmsStorage;
-const watchedList = watchedFilmsStorage.getWathedFilmsList();
-
-// const watchedBtnLib = document.querySelector('.header__libraryWatchBtn');
-// console.log(watchedBtnLib);
-
-// watchedBtnLib.addEventListener('click', onWatchedLibClick);
-
-function onWatchedLibClick(event) {
-    console.log(watchedList);
+export const watchedFilmsStorage = new WatchedFilmsStorage;
+export function onWatchedLibClick() {
+    const watchedList = watchedFilmsStorage.getWathedFilmsList();
     resetMarkup();
-    createPopularMarkup(watchedList);
     appendPopularMarkup(watchedList);
 }
