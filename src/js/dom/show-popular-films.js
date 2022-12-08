80c547d743fef6c879610bfd07e4d1a01b6e6097
aliesia-zahorui <aliesia@ukr.net>
2022-12-08 15:20:35 +0200

    видалення рядка, прропущеного при вирішенні конфлікту

--- src/js/dom/show-popular-films.js
+++ src/js/dom/show-popular-films.js
@@ -2,7 +2,7 @@
 import { filmsApiServise } from '../..';
 import { list, btnScroll } from '../refs';
 import { markupPagination } from './markUpPagination';
-import {showLoader, hideLoader} from './loader';
+import { showLoader, hideLoader } from './loader';
 
 export function createPopularMarkup(data) {
   return data
@@ -86,7 +86,7 @@ export function resetMarkup() {
 export function appendPopularMarkup(data) {
   showLoader();
 
- setTimeout(() => {
+  setTimeout(() => {
     if (!data || data.length === 0) {
       resetMarkup();
       document.querySelector('.pagination__container').style.display = 'none';
@@ -94,7 +94,6 @@ export function appendPopularMarkup(data) {
       hideLoader();
       return;
     }
-    document.querySelector('.pagination__container').style.display = 'none';
     list.insertAdjacentHTML('beforeend', createPopularMarkup(data));
     markupPagination();
     document.querySelector('.pagination__container').style.display = 'block';
@@ -113,9 +112,9 @@ export function appendErrorMessage() {
     '<p>Unable to load images, please try again later.</p>'
   );
 }
-  export function appendEmptyStorageMessage() {
+export function appendEmptyStorageMessage() {
   list.insertAdjacentHTML(
     'beforeend',
     '<p>Hey, could you please add some movies to the list?</p>'
-  )
+  );
 }
