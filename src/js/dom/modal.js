3f4b6ec2c85748ae8fe7d43b7c0e2194455af9aa
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-01-20 22:52:21 +0200

    Update modal.js

--- src/js/dom/modal.js
+++ src/js/dom/modal.js
@@ -1,10 +1,10 @@
 // функції відкриття та закриття з modal.js
 // + рендер карток фільмів
-
-import { filmsApiServise } from "../../index";
-import { list, backdrop, filmcard } from "../refs";
-import TrailerApiService from "../api/getting-trailer";
-
+import { filmsApiServise } from '../../index';
+import { list, backdrop } from '../refs';
+import WatchedFilmsStorage from '../storage/add-to-watced';
+import QueueFilmsStorage from '../storage/add-to-queue';
+import TrailerApiService from '../api/getting-trailer';
 import { watchedFilmsStorage, onWatchedLibClick } from './show-watch-films';
 import { queueFilmsStorage, onQueueLibClick } from './show-queue-films';
 
@@ -26,66 +26,62 @@ const trailerApiService = new TrailerApiService();
 //   return currentFilm;
 // }
 
-list.addEventListener("click", onCardsListClick);
+list.addEventListener('click', createModal);
 
 function createModal(e) {
+  e.preventDefault();
+  const filmCard = e.target.closest('.films__film-card');
 
-    e.preventDefault();
-    const filmCard = e.target.closest(".films__film-card");
+  if (!filmCard) {
+    return;
+  }
 
-    if (!filmCard) {
-        return;
-    }
+  let filmData = null;
+  const filmID = Number(filmCard.dataset.filmsId);
 
-    let filmData = null;
-    const filmID = Number(filmCard.dataset.filmsId);
+  if (filmsApiServise.isWatchedOpen) {
+    watchedFilmsStorage.getWathedFilmsList().map(film => {
+      if (film.id === filmID) {
+        filmData = film;
+      }
+    });
+  } else if (filmsApiServise.isQueueOpen) {
+    queueFilmsStorage.getQueueFilmsList().map(film => {
+      if (film.id === filmID) {
+        filmData = film;
+      }
+    });
+  } else {
+    filmData = filmsApiServise.getFilmById(filmID);
+  }
 
-    if (filmsApiServise.isWatchedOpen) {
-        watchedFilmsStorage.getWathedFilmsList().map(film => {
-            if (film.id === filmID) {
-                filmData = film;
-            }
-        });
-    } else if (filmsApiServise.isQueueOpen) {
-        queueFilmsStorage.getQueueFilmsList().map(film => {
-            if (film.id === filmID) {
-                filmData = film;
-            }
-        });
-    } else {
-        filmData = filmsApiServise.getFilmById(filmID);
-    } 
+  let filmGenresNames = 'unknown';
+  if (filmData.genre_ids) {
+    filmGenresNames = getFilmGenresNames(filmData.genre_ids);
+  }
+  makeFilmcardMarkup(filmData, filmGenresNames);
+  openModal();
 
-    let filmGenresNames = "unknown";
-    if (filmData.genre_ids) {
-        filmGenresNames = getFilmGenresNames(filmData.genre_ids); 
-    }
-    makeFilmcardMarkup(filmData, filmGenresNames);
-    openModal();
+  //-----Для кнопок
+  currentFilm = filmData;
+  addToWatchedBtn = document.querySelector('.btn__modal-add');
+  addToQueueBtn = document.querySelector('.btn__modal-queue');
+  removeFromQueueBtn = document.querySelector('.btn__modal-r-queue');
+  removeFromWatchedBtn = document.querySelector('.btn__modal-r-watched');
 
-//-----Для кнопок
-    currentFilm = filmData;
-    addToWatchedBtn = document.querySelector('.btn__modal-add');
-    addToQueueBtn = document.querySelector('.btn__modal-queue');
-    removeFromQueueBtn = document.querySelector('.btn__modal-r-queue');
-    removeFromWatchedBtn = document.querySelector('.btn__modal-r-watched');
-    
+  addToWatchedBtn.addEventListener('click', addToWatchedLS);
+  removeFromWatchedBtn.addEventListener('click', removeFromWatchedLS);
+  addToQueueBtn.addEventListener('click', addToQueueLS);
+  removeFromQueueBtn.addEventListener('click', removeFromQueueLS);
 
-    addToWatchedBtn.addEventListener('click', addToWatchedLS);
-    removeFromWatchedBtn.addEventListener('click', removeFromWatchedLS);
-    addToQueueBtn.addEventListener('click', addToQueueLS);
-    removeFromQueueBtn.addEventListener('click', removeFromQueueLS);
-    
-
-    if (watchedFilmsStorage.checkFilmInWatchedLocStor(currentFilm)) {
-        addToWatchedBtn.classList.add('is-hidden');
-        removeFromWatchedBtn.classList.remove('is-hidden');
-    }
-    if (queueFilmsStorage.checkFilmInQueueLocStor(currentFilm)) {
-        addToQueueBtn.classList.add('is-hidden');
-        removeFromQueueBtn.classList.remove('is-hidden');
-    }
-
+  if (watchedFilmsStorage.checkFilmInWatchedLocStor(currentFilm)) {
+    addToWatchedBtn.classList.add('is-hidden');
+    removeFromWatchedBtn.classList.remove('is-hidden');
+  }
+  if (queueFilmsStorage.checkFilmInQueueLocStor(currentFilm)) {
+    addToQueueBtn.classList.add('is-hidden');
+    removeFromQueueBtn.classList.remove('is-hidden');
+  }
 }
 
 function getFilmGenresNames(filmGenresID) {
@@ -100,7 +96,6 @@ function getFilmGenresNames(filmGenresID) {
 }
 
 function makeFilmcardMarkup(filmData, filmGenresNames) {
-
   const {
     poster_path,
     title,
@@ -113,21 +108,7 @@ function makeFilmcardMarkup(filmData, filmGenresNames) {
     backdrop_path,
   } = filmData;
 
-  let poster300 = `https://image.tmdb.org/t/p/w300/${poster_path}`;
-  let poster500 = `https://image.tmdb.org/t/p/w500/${poster_path}`;
-  let poster780 = `https://image.tmdb.org/t/p/w780/${poster_path}`;
-  let poster1280 = `https://image.tmdb.org/t/p/w1280/${poster_path}`;
-  let poster2000 = `https://image.tmdb.org/t/p/original/${poster_path}`;
-  if (poster_path === null) {
-    poster300 = `https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?s=612x612&w=0&k=20&c=JN4E5qJgcq3qm89rSc2BIJT6AZ80MvRJie__r3OENY8=`;
-    poster500 = `https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?s=612x612&w=0&k=20&c=JN4E5qJgcq3qm89rSc2BIJT6AZ80MvRJie__r3OENY8=`;
-    poster780 = `https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?s=612x612&w=0&k=20&c=JN4E5qJgcq3qm89rSc2BIJT6AZ80MvRJie__r3OENY8=`;
-    poster1280 = `https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?s=612x612&w=0&k=20&c=JN4E5qJgcq3qm89rSc2BIJT6AZ80MvRJie__r3OENY8=`;
-    poster2000 = `https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?s=612x612&w=0&k=20&c=JN4E5qJgcq3qm89rSc2BIJT6AZ80MvRJie__r3OENY8=`;
-  }
-
   const backdropEl = `
-
                     <div class='backdrop__img' 
                         style="background-image:
                             linear-gradient(to right, rgba(47, 48, 58, 0.4), rgba(47, 48, 58, 0.4)),
@@ -150,20 +131,17 @@ function makeFilmcardMarkup(filmData, filmGenresNames) {
                                     </button>
                                 </div>
                                 <img class="filmcard__img"  
-
-                                srcset="
-                                ${poster300}   300w,
-                                ${poster500}   500w,
-                                ${poster780}   780w,
-                                ${poster1280}   1280w,
-                                ${poster2000} 2000w
-                                
-                            "
-                            loading="lazy"
-                            src="${poster300}"
-                            alt="Poster of the film ${title}"
-                      alt="#"
-
+                                    srcset="
+                                        https://image.tmdb.org/t/p/w300/${poster_path}   300w,
+                                        https://image.tmdb.org/t/p/w500/${poster_path}   500w,
+                                        https://image.tmdb.org/t/p/w780/${poster_path}   780w,
+                                        https://image.tmdb.org/t/p/w1280/${poster_path}   1280w,
+                                        https://image.tmdb.org/t/p/original/${poster_path} 2000w
+                                        
+                                    "
+                                    loading="lazy"
+                                    src="https://image.tmdb.org/t/p/w300/${poster_path}"
+                                    alt="#"
                                     sizes="(max-width: 320px) 280px,
                                         (max-width: 768px) 340px,
                                         400px"/>
@@ -176,16 +154,16 @@ function makeFilmcardMarkup(filmData, filmGenresNames) {
                                     <tr>
                                         <td>Vote / Votes</td>
                                         <td>                                   
-
-                                            <div class="filmcard__votes"><span class="filmcard__vote">${vote_average.toFixed(1)}</span> / ${vote_count}</div>
-
+                                            <div class="filmcard__votes"><span class="filmcard__vote">${vote_average.toFixed(
+                                              1
+                                            )}</span> / ${vote_count}</div>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td>Popularity</td>
-
-                                        <td colspan="3">${popularity.toFixed(1)}</td>
-
+                                        <td colspan="3">${popularity.toFixed(
+                                          1
+                                        )}</td>
                                     </tr>
                                     <tr>
                                         <td>Original Title</td>
@@ -214,17 +192,15 @@ function makeFilmcardMarkup(filmData, filmGenresNames) {
                         </button>
                     </div>`;
 
-           
-    backdrop.innerHTML = backdropEl;
+  backdrop.innerHTML = backdropEl;
 }
 
-function openModal () {
-    backdrop.classList.remove("is-hidden");
-    document.body.style.overflow = 'hidden';
-    backdropImg = document.querySelector('.backdrop__img');
-    modalCloseBtn = document.querySelector('.modal-close-btn');
-    youTubeBtn = document.querySelector('.youtube-btn');
-
+function openModal() {
+  backdrop.classList.remove('is-hidden');
+  document.body.style.overflow = 'hidden';
+  backdropImg = document.querySelector('.backdrop__img');
+  modalCloseBtn = document.querySelector('.modal-close-btn');
+  youTubeBtn = document.querySelector('.youtube-btn');
 
   modalCloseBtn.addEventListener('click', closeModal);
   document.addEventListener('click', closeModalByOutBackdropClick);
@@ -234,7 +210,7 @@ function openModal () {
 
 function closeModal() {
   backdrop.classList.add('is-hidden');
-  document.body.style.overflow = 'scroll';
+  document.body.style.overflow = 'auto';
 
   modalCloseBtn.removeEventListener('click', closeModal);
   document.removeEventListener('click', closeModalByOutBackdropClick);
@@ -243,11 +219,9 @@ function closeModal() {
 }
 
 function closeModalByOutBackdropClick(e) {
-
-    if (e.target === backdropImg || e.target === backdrop ) {
-        closeModal();
-    }
-
+  if (e.target === backdropImg || e.target === backdrop) {
+    closeModal();
+  }
 }
 
 function closeModalByEsc(e) {
@@ -302,58 +276,4 @@ function removeFromQueueLS() {
   if (filmsApiServise.isQueueOpen) {
     onQueueLibClick();
   }
-
-}
-
-function onCardsListClick(e) {
-    if (e.target.classList.contains("films__overlay")) {
-          return 
-    };
-    if (e.target.nodeName === 'BUTTON') {
-
-    if (e.target.dataset.type === "w-add") {
-        addToWatchedLS();
-
-    } else if (e.target.dataset.type === "q-add") {
-        addToQueueLS();
-        
-    } else if (e.target.dataset.type === "w-remove") {
-        removeFromWatchedLS();
-
-    } else if (e.target.dataset.type === "q-remove") {
-       removeFromQueueLS();
-    } return
-    }
-    createModal(e);
-};
-
-export function checkButtonsOnHover(e) {
-      const filmCard = e.target.closest(".films__film-card");
-    if (!filmCard) {
-        return;
-    }
-
-    const filmID = Number(filmCard.dataset.filmsId);
-    const filmData = filmsApiServise.getFilmById(filmID);
-
-    currentFilm = filmData;
-    addToWatchedBtn = filmCard.querySelector('.btn__card-add');
-    addToQueueBtn = filmCard.querySelector('.btn__card-queue');
-    removeFromQueueBtn = filmCard.querySelector('.btn__card-r-queue');
-    removeFromWatchedBtn = filmCard.querySelector('.btn__card-r-watched');
-
-    if (watchedFilmsStorage.checkFilmInWatchedLocStor(currentFilm)) {
-        addToWatchedBtn.classList.add('is-hidden');
-        removeFromWatchedBtn.classList.remove('is-hidden');
-    } else {
-         addToWatchedBtn.classList.remove('is-hidden');
-        removeFromWatchedBtn.classList.add('is-hidden');
-    }
-    if (queueFilmsStorage.checkFilmInQueueLocStor(currentFilm)) {
-        addToQueueBtn.classList.add('is-hidden');
-        removeFromQueueBtn.classList.remove('is-hidden');
-    } else {
-        addToQueueBtn.classList.remove('is-hidden');
-        removeFromQueueBtn.classList.add('is-hidden');
-    }
 }
